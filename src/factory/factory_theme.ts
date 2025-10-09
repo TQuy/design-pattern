46ddce015526e8b5b94bef5ad1a63a5c630c34a3
quynt <quynguyenthe000@gmail.com>
2025-10-09 08:13:14 +0700

    add factory_theme

---
+++ src/factory/factory_theme.ts
@@ -0,0 +1,73 @@
+interface Button {
+  render(): void;
+}
+
+interface TextField {
+  render(): void;
+}
+
+interface UIFactory {
+  createButton(): Button;
+  createTextField(): TextField;
+}
+
+class LightButton implements Button {
+  render(): void {
+    console.log("Rendering Light Button");
+  }
+}
+
+class DarkButton implements Button {
+  render(): void {
+    console.log("Rendering Dark Button");
+  }
+}
+
+class LightTextField implements TextField {
+  render(): void {
+    console.log("Rendering Light TextField");
+  }
+}
+
+class DarkTextField implements TextField {
+  render(): void {
+    console.log("Rendering Dark TextField");
+  }
+}
+
+class LightUIFactory implements UIFactory {
+  createButton(): Button {
+    return new LightButton();
+  }
+  createTextField(): TextField {
+    return new LightTextField();
+  }
+}
+
+class DarkUIFactory implements UIFactory {
+  createButton(): Button {
+    return new DarkButton();
+  }
+  createTextField(): TextField {
+    return new DarkTextField();
+  }
+}
+
+class UIApp {
+  constructor(private themeFactory: UIFactory) {
+    this.themeFactory = themeFactory;
+  }
+
+  renderUI() {
+    const button = this.themeFactory.createButton();
+    button.render();
+    const textField = this.themeFactory.createTextField();
+    textField.render();
+  }
+}
+
+const darkApp = new UIApp(new DarkUIFactory());
+darkApp.renderUI();
+
+const lightApp = new UIApp(new LightUIFactory());
+lightApp.renderUI();
